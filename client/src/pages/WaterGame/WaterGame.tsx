bbd64af1ece2ca207e2cc9d7133a5d635cf5d208
kathyychenn <ktchen@ucsd.edu>
2026-02-10 13:02:00 -0800

    implement frontend for welcome page and tutorial modal

--- client/src/pages/WaterGame/WaterGame.tsx
+++ client/src/pages/WaterGame/WaterGame.tsx
@@ -1,75 +1,78 @@
+import { useState } from "react";
 import { Popup } from "../../components/Popup";
+import styles from "./WaterGame.module.css";
 
 export const WaterGame = () => {
+  const [showStart, setShowStart] = useState(true);
+  const [showTutorial, setShowTutorial] = useState(false);
+  const [showGame, setShowGame] = useState(false);
+  const [showEnd, setShowEnd] = useState(false);
+
   return (
-    <div>
-      <h1>Water Game</h1>
-      <Popup
-        variant="water"
-        screen="start"
-        header="Welcome To"
-        buttonText="Play"
-        title="RAINDROP RUSH"
-        onClick={() => (window.location.href = "/")}
-      />
-      <Popup
-        variant="soil"
-        screen="start"
-        header="Welcome To"
-        buttonText="Play"
-        title="DOWN TO THE ROOTS"
-        onClick={() => (window.location.href = "/")}
-      />
-      <Popup
-        variant="water"
-        screen="tutorial"
-        header="How To Play"
-        buttonText="I'm Ready"
-        onClick={() => (window.location.href = "/")}
-        textList={[
-          "Click and drag your bucket under a raindrop to collect it",
-          "Collect the raindrop which correctly answers the question at the top of the screen",
-          "Goal: Collect as many raindrops as you can!",
-        ]}
-      />
-      <Popup
-        variant="soil"
-        screen="tutorial"
-        header="How To Play"
-        buttonText="I'm Ready"
-        onClick={() => (window.location.href = "/")}
-        textList={[
-          "You are in a dungeon underground with various rooms and have been given 3 quests",
-          "Each room may contain an answer to a quest",
-          "Type the number of the correct corresponding quest to collect that answer",
-          "Goal: Collect the answers to all 3 quests",
-        ]}
-      />
-      <Popup
-        variant="water"
-        screen="end"
-        header="Time's Up"
-        buttonText="Go Back to Home"
-        onClick={() => (window.location.href = "/")}
-        textList={[
-          "Number of correctly answered questions: 7",
-          "Number of incorrectly answered questions: 3",
-          "Total number of points earned: 7",
-        ]}
-      />
-      <Popup
-        variant="soil"
-        screen="end"
-        header="Mission Complete"
-        buttonText="Go Back to Home"
-        onClick={() => (window.location.href = "/")}
-        textList={[
-          "You are in a dungeon underground with various rooms and have been given 3 quests",
-          "Each room may contain an answer to a quest",
-          "Type the number of the correct corresponding quest to collect that answer",
-          "Goal: Collect the answers to all 3 quests",
-        ]}
-      />{" "}
+    <div className={styles.gameContainer}>
+      {showStart && (
+        <Popup
+          variant="water"
+          screen="start"
+          header="Welcome To"
+          buttonText="Play"
+          title="RAINDROP RUSH"
+          onClick={() => {
+            setShowStart(false);
+            setShowTutorial(true);
+          }}
+        />
+      )}
+      {showTutorial && (
+        <Popup
+          variant="water"
+          screen="tutorial"
+          header="How To Play"
+          buttonText="I'm Ready"
+          onClick={() => {
+            setShowTutorial(false);
+            setShowGame(true);
+          }}
+          textList={[
+            "A question will appear at the top of the screen",
+            "Raindrops will fall, each with a possible answer",
+            "Catch the correct answer to earn a point",
+            "Move the bucket left and right using the arrow keys on your keyboard",
+            "Goal: Collect as many raindrops as you can to gather water for your tree!",
+          ]}
+        />
+      )}
+      {showGame && (
+        <div className={styles.gameScreen}>
+          <span className={styles.question}>
+            <p className={styles.questionText}>
+              What is the chemical formula for water?
+            </p>
+          </span>
+          <button
+            onClick={() => {
+              setShowGame(false);
+              setShowEnd(true);
+            }}
+          >
+            End Game
+          </button>
+        </div>
+      )}
+      {showEnd && (
+        <Popup
+          variant="water"
+          screen="end"
+          header="Time's Up"
+          buttonText="Go Back to Home"
+          onClick={() => (window.location.href = "/")}
+          textList={[
+            "Number of correctly answered questions: 7",
+            "Number of incorrectly answered questions: 3",
+            "Total number of points earned: 7",
+          ]}
+        />
+      )}
     </div>
   );
 };
