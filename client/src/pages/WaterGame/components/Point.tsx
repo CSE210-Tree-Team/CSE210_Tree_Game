3b8b5d2bc6933c21c4bd8b5722bc81446c45eef5
kathyychenn <ktchen@ucsd.edu>
2026-02-25 22:53:38 -0800

    add comments to address adrian's feedback

--- client/src/pages/WaterGame/components/Point.tsx
+++ client/src/pages/WaterGame/components/Point.tsx
@@ -1,3 +1,12 @@
+/*
+Point Component
+
+This component displays a score indicator when the player catches a raindrop
+with the bucket. It renders a "+" or "X" score text styled in green or red
+depending on whether the caught raindrop answer was correct or incorrect.
+The Point is positioned absolutely on the game screen above the bucket.
+*/
+
 import styles from "./Point.module.css";
 import { NUM_POINTS } from "../constants";
 import { INCORRECT } from "../constants";
